e1bf4dcef7c215ed3aa211a6357e0529f8e6232d
Marat Ospanov <marat.ospanov@gmail.com>
2014-07-13 14:10:21 -0300

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrottenpees::Application.config.secret_token = '9a46451dc189366f52602dce48c4a89855150f25407b5af5a5bb58eb44086183d81257046e55a877e8c50edab3e14dfed4d8a8b54aed772e1a2f1509e4a8e62d'
